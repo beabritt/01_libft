4ddcace6ef409c1c06fa6c055350767a2331972c
Beatriz Camino Márquez <britt@iMac-de-Beatriz.home>
2023-02-15 14:24:43 +0100

    libft

---
+++ work/ft_calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+
+void    *ft_calloc(size_t n, size_t size)
+{
+   size_t   x;
+   void     *p;
+
+	x = n * size;
+	p = malloc(x);
+	if (!p)
+		return (NULL);
+	ft_bzero(p, x);
+	return (p);
+}
+
+int main(void)
+{
+    size_t  n;
+    size_t  size;
+	void	*p;
+
+	n = 2;
+	size = 4;
+	p = ft_calloc(n, size);
+    printf("%lu \n", sizeof(p));
+	free(p);
+	return (0);
+}
