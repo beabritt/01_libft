3c9fb90771026ff4067f11f53f63abce3516e30b
Beatriz Camino Márquez <britt@iMac-de-Beatriz.home>
2023-02-16 20:22:01 +0100

    libft

---
+++ work/ft_strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+/*size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dst, const void *src, size_t n);*/
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	x;
+	char	*s2;
+
+	x = ft_strlen(s1) + 1;
+	s2 = (char *)malloc(x);
+	if (!s2)
+		return (NULL);
+	ft_memcpy((void *)s2, (const void *)s1, x);
+	return ((char *)s2);
+}
+
+/*int	main(void)
+{
+	char	s [8] = "Soy Bea";
+	char	*s2;
+	
+	s2 = ft_strdup(s);
+	printf("%s \n", s2);
+	free (s2);
+	return (0);
+}*/
---
+++ work/ft_substr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "libft.h"
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+
+}
+
+int main(void)
+{
+
+
+    return (0);
+}
