66d9af401d6370d98668a74b47403a23fda8cea2
Beatriz Camino Márquez <britt@iMac-de-Beatriz.home>
2023-02-17 18:40:04 +0100

    libft

--- work/pruebas.c
+++ work/pruebas.c
@@ -6,31 +6,6 @@
 //ft_strlcpy(subs, &s[start], (size_t)len + 1);
 
 
-char
-    *substr(char const *s, unsigned int start, size_t len)
-{
-    size_t    i;
-    size_t    j;
-    char    *str;
-
-    str = (char*)malloc(sizeof(*s) * (len + 1));
-    if (!str)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (s[i])
-    {
-        if (i >= start && j < len)
-        {
-            str[j] = s[i];
-            j++;
-        }
-        i++;
-    }
-    str[j] = 0;
-    return (str);
-}
-
 int main(void)
 {
     char const        s [15] = "Soy Bea Camino";
