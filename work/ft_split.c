bac1e8c083d71ab10cc4e137f423e62e38a3d113
Beatriz Camino Marquez <becamino@c2r10s2.42madrid.com>
2023-03-01 17:48:55 +0100

    libft

--- work/ft_split.c
+++ work/ft_split.c
@@ -15,11 +15,8 @@
 #include<stdlib.h>
 #include "libft.h"
 
-static int	ft_search(const char *s, char c)
+static int	ft_search(const char *s, char c, int x)
 {
-	size_t	x;
-
-	x = 0;
 	while (s[x] != '\0' && s[x] != c)
 		x++;
 	return (x);
@@ -45,47 +42,50 @@ static size_t	ft_wcounter(char const *s, char c)
 
 char	**ft_split(char const *s, char c)
 {
-	size_t 	x;
-	size_t	y;
-	size_t	wc;
-	size_t	a;
-	char	**arr;
+    int       x;
+    size_t    y;
+    size_t    wc;
+    size_t    a;
+    char    **arr;
 
-	if (!s)
-		return (0);
-	wc = ft_wcounter(s, c);
-	arr = ft_calloc(wc, sizeof(char*));
-	if (!arr)
-		return (0);
-	x = -1;
-	while (s[x] != '\0')
-	{	
-		while (s[x + 1] != '\0' && s[x] == c)
-			x++;
-		if (x < 0)
-			y = 0;
-		else
-			y = x;
-		x = ft_search(s[x + 1], c);
-		a = 0;
-		arr[a] = ft_substr(s, y, x - y + 1);
-		a++;
-		if (!arr[a])
-			while (arr[a])
-				free (arr[a--]);
-	}
-	arr[a] = '\0';
-	return (arr);
+    if (!s)
+        return (0);
+    wc = ft_wcounter(s, c);
+    arr = (char **)calloc(wc + 1, sizeof(char *));
+    if (!arr)
+        return (0);
+    x = 0;
+    a = 0;
+    while (s[x] != '\0')
+    {    
+        while (s[x] != '\0' && s[x] == c)
+            x++;
+        if (x < 0)
+            y = 0;
+        else
+            y = x;
+        x = ft_search(s, c, x);
+        arr[a] = ft_substr(s, y, x - y);
+        if (!arr[a])
+            while (arr[a])
+                free (arr[a--]);
+        a++;
+    }
+   	arr[a] = calloc(1, sizeof(char));
+    return (arr);
 }
 
 int	main(void)
 {
 	char	s[14] = "Que puto frio";
 	char	c;
-	char	*array;
+	char	**array;
 
 	c ='p';
 	array = ft_split(s, c);
+	printf("%s \n", array[0]);
+	printf("%s \n", array[1]);
+	printf("%s \n", array[2]);
 	free (array);
 	return (0);
 }
--- work/pruebas.c
+++ work/pruebas.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "libft.h"
+#include "libft.h"
 
-#include<unistd.h>
+static int	ft_search(const char *s, char c)
+{
+	size_t	x;
 
-static int	ft_wcounter(char const *s, char c)
+	x = 0;
+	while (s[x] != '\0' && s[x] != c)
+		x++;
+	return (x);
+}
+
+static size_t	ft_wcounter(char const *s, char c)
 {
 	size_t	x;
 	size_t	cont;
@@ -22,11 +30,62 @@ static int	ft_wcounter(char const *s, char c)
 	return (cont);
 }
 
-int main() 
+static char ft_strs(const char *s, int x, size_t y)
 {
-	size_t	cont;
+	size_t	a;
+	char	*arr;
+	a = 0;
+	arr[a] = ft_substr(s, y, x - y + 1);
+	a++;
+	if (!arr[a])
+		while (arr[a])
+			free (arr[a--]);
+	if (s[x] = '\0')
+		arr[a] = ft_calloc(1, sizeof(char));
+		arr[a][0] = '\0';
+}
 
-	cont = ft_wcounter("Que puto frio", 'p');
-	printf("%zu \n", cont);
+char	**ft_split(char const *s, char c)
+{
+	int 	x;
+	size_t	y;
+	size_t	wc;
+	size_t	a;
+	char	**arr;
+
+	if (!s)
+		return (0);
+	wc = ft_wcounter(s, c);
+	arr = (char**)ft_calloc(wc, sizeof(char));
+	if (!arr)
+		return (0);
+	x = -1;
+	while (s[x] != '\0')
+	{	
+		while (s[x + 1] != '\0' && s[x] == c)
+			x++;
+		if (x < 0)
+			y = 0;
+		else
+			y = x;
+		x = ft_search(&s[x + 1], c);
+		a = 0;
+		while (a <= wc)
+		{
+			arr[a] = ft_strs(s, x, y);
+			a++;
+		}
+	return (arr);
+}
+
+int	main(void)
+{
+	char	s[14] = "Que puto frio";
+	char	c;
+	char	**array;
+
+	c ='p';
+	array = ft_split(s, c);
+	free (array);
 	return (0);
 }
